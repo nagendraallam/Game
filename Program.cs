f0e94d917d531e60ac0fda8f941c7232467cb18d
Nagendra Allam <nagendraallam@icloud.com>
2026-01-17 00:59:16 +0530

    Added raylib

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ZeroDay.Base
+﻿using Raylib_cs;
+
+namespace ZeroDay.Base
 {
     class Program
     {
+        [System.STAThread]
         public static void Main()
         {
+            Raylib.InitWindow(640, 400, "Fun Game");
             Console.WriteLine("Building world...");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Starting...", 12, 12, 20, Color.Black);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
     }
 }
